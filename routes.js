d996d78624e378844fe7ca03a42e0bc9d2fee61e
Lucas Baron <lucasbaron28@hotmail.com>
2021-03-18 14:36:21 -0300

    Validando dados

--- routes.js
+++ routes.js
@@ -14,6 +14,13 @@ routes.get('/instructors/create', function(req, res){
 })
 
 routes.post('/instructors', function(req, res){
+    const keys = Object.keys(req.body)
+
+    for (key of keys){
+        if (req.body[key] == "")
+            return res.send('Please, fill all fields!')
+    }
+
     return res.send(req.body)
 })
 
