6f5dd01cce7594ffc9e3c578403bd2a43dd53442
Lucas Baron <lucasbaron28@hotmail.com>
2021-03-19 14:01:04 -0300

    Idade

--- routes.js
+++ routes.js
@@ -3,18 +3,18 @@ const routes = express.Router()
 const instructors = require('./instructors')
 
 routes.get('/', function(req, res){
-    return res.render('instructors')
+    return res.redirect('/instructors')
 })
 
 routes.get('/instructors', function(req, res){
     return res.render('instructors/index')
 })
 
-routes.get('/instructors/:id', instructors.show)
-
 routes.get('/instructors/create', function(req, res){
     return res.render('instructors/create')
 })
+
+routes.get('/instructors/:id', instructors.show)
 
 routes.post('/instructors', instructors.post)
 
